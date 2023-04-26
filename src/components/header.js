05159496ad158427ad3cec644542ba3c19ae8dd1
SaToGo <stoledano97@gmail.com>
2023-04-26 02:39:22 +0200

    Add header.js component

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+
+const Header = () => {
+
+    return (
+        <header>
+            <div>
+                <button>
+                    <h1>Main Page</h1>
+                </button>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
