eef08df70c7bacdd3590cd60cedcaa6e37c9a58e
SaToGo <stoledano97@gmail.com>
2023-06-24 01:45:25 +0200

    add total price to cart, and button buy
    Add a total price element that is yet not functional.
    
    add button buy that shows an alert stating that you cant
    buy on a fake shop.

--- src/Layout/Cart.js
+++ src/Layout/Cart.js
@@ -6,7 +6,7 @@ const Cart = ({ isCartVisible, handleCart, cart }) => {
     return (
         <>
             {isCartVisible === true && 
-                <section className="cart">
+                <section className='cart'>
                     <div className='cart__container'>
                         <h2 className='cart__sectionTitle'>Your Cart</h2>
                         <ul>
@@ -17,10 +17,22 @@ const Cart = ({ isCartVisible, handleCart, cart }) => {
                                 
                             }
                         </ul>
+                        <hr className='cart__hr' />
+                        <div className='cart__totalPrice'>
+                            <p>TODO</p>
+                            <h3>Total Price:</h3>
+                            <p>$200</p>
+                        </div>
+                        <div>
+                            <ButtonShop 
+                                click={() => alert('fake store you can\'t buy')}
+                                text='Buy' 
+                            />
+                        </div>
                         <div>
                             <ButtonShop 
                                 click={handleCart}
-                                text="Close" 
+                                text='Close' 
                             />
                         </div>
                     </div>
