d520cce3a6de2f89f68d0c7b5b66544714155dca
SaToGo1 <stoledano97@gmail.com>
2023-11-11 16:19:23 +0100

    Animation for the cart and responsive.
    Animation to the opening of the cart.
    
    Responsive to mobile when we had the mobile on it's side, wide.

--- src/views/Layout/Cart.js
+++ src/views/Layout/Cart.js
@@ -1,13 +1,25 @@
 import CartCard from '../components/CartCard';
 import ButtonShop from '../components/ButtonShop';
 import './Cart.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import confetti from 'canvas-confetti';
 import useCart from '../../hooks/useCart';
 
 const Cart = () => {
     const [totalPrice, setTotalPrice] = useState(0)
     const { cart, isCartVisible, handleCartDisplay } = useCart()
+
+    // Cart Transition
+    useEffect(() => {
+        const cartElement = document.querySelector('.cart');
+        if(isCartVisible && cartElement) {
+            setTimeout(() => {
+                cartElement.classList.toggle('cart-transition');
+            }, 1)
+        } else {
+            // cartElement.classList.toggle('cart-transition');
+        }
+    }, [isCartVisible])
 
     const calculatePrice = () => {
         let price = cart.reduce((sum, el) => sum + (el.price * el.quantity), 0)
@@ -46,13 +58,13 @@ const Cart = () => {
                             <h3>Total Price:</h3>
                             <p>${totalPrice}</p>
                         </div>
-                        <div>
+                        <div className='cart__buyDiv'>
                             <ButtonShop 
                                 click={() => confetti()}
                                 text='Buy' 
                             />
                         </div>
-                        <div>
+                        <div className='cart__closeDiv'>
                             <ButtonShop 
                                 click={handleCartDisplay}
                                 text='Close' 
